2563d7f9ab44cf2e843b731d8f6483bea33fde14
Ransford <ransfordaddai111@gmail.com>
2024-10-25 10:56:31 -0800

    version 1 updated

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
 print('app.py')
-print('This is version 1.')
-print('This is version 2')
-print('This is version 3')
-print('This is version 4')
+print('This is version 1')
--- src/models.py
+++ src/models.py
@@ -1,5 +1,2 @@
 print('Models.py')
 print('This is version 1')
-print('This is version 2')
-print('This is version 3')
-print('This is version 4')
