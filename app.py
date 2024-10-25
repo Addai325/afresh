00cac477e21a55ac2fda9539a5a9c0c3a885535a
Ransford <ransfordaddai111@gmail.com>
2024-10-25 10:50:58 -0800

    version 3

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print('app.py')
 print('This is version 1.')
 print('This is version 2')
+print('This is version 3')
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 print('config.py')
 print('This is version 1')
 print('This is version 2')
+print('This is version 3')
